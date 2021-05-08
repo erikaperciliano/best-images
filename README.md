988cc5777b698b93e16066d98f5efdf1401ad206
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-05-08 15:17:34 -0300

    add project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Best Images 💥
+
+### This is an image management system, allowing the user to register photos and search for those that follow a certain criterion.
+
+
+
+## 🛠 Technology
+- [Vue.js][vuejs]
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="photo of photos">
+        <img :src="photo.url" :alt="photo.title" />
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title: 'Best Images',
+      photos: [
+        {
+          url: 'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGRvZ3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+          title: 'dog'
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1514984879728-be0aff75a6e8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTd8fGRvZ3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+          title: 'dog2'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
