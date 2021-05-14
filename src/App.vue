f150d01c2a477059ea561686bcff716d6b7b9a97
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-05-14 09:11:34 -0300

    initializing component styling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <h1>{{title}}</h1>
-    <ul>
-      <li v-for="photo of photos">
-        <img :src="photo.url" :alt="photo.title" />
+  <div class="body">
+    <h1 class="centralized">{{title}}</h1>
+    <ul class="photoList">
+      <li class="photo-list-item" v-for="photo of photos">
+        <div class="panel">
+          <h2 class="panel-title">{{photo.titulo}}</h2>
+          <div class="panel-container">
+            <img class="responsive-image" :src="photo.url" :alt="photo.title" />
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -28,6 +33,46 @@ export default {
 </script>
 
 <style>
+  .body {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+  .centralized {
+    text-align: center;
+  }
 
+  .photoList {
+    list-style: none;
+  }
+
+  .photoList .photo-list-item {
+      display: inline-block;
+  }
+
+  .responsive-image {
+    width: 100%;
+  }
+
+  .panel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .panel .panel-title {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
 
