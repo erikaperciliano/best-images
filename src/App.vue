072d5dee40f2df073979fa019b3bb9160e1771b8
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-05-14 09:33:03 -0300

    creating components

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,9 @@
     <h1 class="centralized">{{title}}</h1>
     <ul class="photoList">
       <li class="photo-list-item" v-for="photo of photos">
-        <div class="panel">
-          <h2 class="panel-title">{{photo.titulo}}</h2>
-          <div class="panel-container">
-            <img class="responsive-image" :src="photo.url" :alt="photo.title" />
-          </div>
-        </div>
+        <my-panel :title="photo.titulo">
+          <img class="responsive-image" :src="photo.url" :alt="photo.title" />
+        </my-panel>
       </li>
     </ul>
   </div>
@@ -16,7 +13,14 @@
 </template>
 
 <script>
+import Panel from './components/shared/panel/Panel.vue';
+
 export default {
+
+  components: {
+    'my-panel': Panel
+  },
+
   data(){
     return {
       title: 'Best Images',
@@ -52,27 +56,6 @@ export default {
 
   .responsive-image {
     width: 100%;
-  }
-
-  .panel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .panel .panel-title {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
   }
 </style>
 
