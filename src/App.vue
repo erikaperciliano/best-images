21147ffab79d4dfb8ee59d74540db1a8d97ecb6e
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-05-14 23:07:37 -0300

    add transition on routes

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,9 @@
         </li>
       </ul>
     </nav>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 
 </template>
@@ -31,6 +33,14 @@ export default {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+
+   .page-enter, .page-leave-active {
+    opacity: 0;
+  }
+
+  .page-enter-active, .page-leave-active {
+    transition: opacity .4s;
   }
 </style>
 
