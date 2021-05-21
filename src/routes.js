833aee1bd3733e15935203c19557950dafbec676
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-05-21 20:30:33 -0300

    naming menu item

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import Register from './components/register/Register.vue';
 import Home from './components/home/Home.vue';
 
 export const routes = [
-  {path: '', name: 'home', component: Home, title: 'Home'},
-  {path: '/register', name: 'register', component: Register, title: 'Register'},
-  {path: '*', component: Home}
+  {path: '', name: 'home', component: Home, title: 'Home', menu: true},
+  {path: '/register', name: 'register', component: Register, title: 'Register', menu: true},
+  {path: '*', component: Home, menu: true}
 ];
