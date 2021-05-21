0579f888c2a3753c750cdd6383945c42376c2ebd
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-05-21 20:28:05 -0300

    redirects you to home if you enter a nonexistent address

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ import Home from './components/home/Home.vue';
 
 export const routes = [
   {path: '', name: 'home', component: Home, title: 'Home'},
-  {path: '/register', name: 'register', component: Register, title: 'Register'}
+  {path: '/register', name: 'register', component: Register, title: 'Register'},
+  {path: '*', component: Home}
 ];
